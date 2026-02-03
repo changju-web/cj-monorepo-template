78b4571ab3d424dadfd33086b8aa10c5750eb3ef
wangjiahui <13226651554@163.com>
2026-02-03 09:57:10 +0800

    style: 更新格式化配置

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,8 +2,21 @@
 
 /** @type {import("prettier").Config} */
 export default {
-  bracketSpacing: true,
-  singleQuote: false,
-  arrowParens: "avoid",
-  trailingComma: "none"
-};
+  /** 换行长度 */
+  printWidth: 100,
+
+  /** tab 宽度 */
+  tabWidth: 2,
+
+  /** 在语句末尾强制分号 */
+  semi: false,
+
+  /** 强制使用单引号 */
+  singleQuote: true,
+
+  /** 结束换行符号类型 */
+  endOfLine: 'auto',
+
+  /** 尾随逗号模式 */
+  trailingComma: 'none'
+}
